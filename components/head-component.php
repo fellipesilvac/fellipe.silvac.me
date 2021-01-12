57ce5ea91c1eef5ab1f5d6af3118925283cffde9
Fellipe Silvac <59098400+fellipesilvac@users.noreply.github.com>
2021-01-11 23:21:26 -0300

    Add files via upload

---
+++ components/head-component.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-language" content="pt-br">
+    <meta charset="utf-8"/>
+	<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<title>Fellipe Silvac</title>
+
+    <!-- CSS e Personalização -->
+    <link rel="stylesheet" href="./style/style.css">	
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+ 
+    <!-- FavIcons -->
+    <!-- <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/metatag/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/metatag/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/metatag/favicon-16x16.png">
+    <link rel="mask-icon" href="/assets/images/metatag/safari-pinned-tab.svg" color="#7854f6">
+    <meta name="msapplication-TileColor" content="#7854f6">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="theme-color" content="#121212"> -->
+
+
+    <!-- PWA -->
+    <!-- <link rel="manifest" href="/manifest.json"> -->
+
+    <!-- Statusbar do IOS Transparente -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+    <!-- OWL Carossel -->
+    <!-- <link rel='stylesheet' href='../../lib/owlCarousel/dist/assets/owl.carousel.min.css'>
+    <link rel='stylesheet' href='../../lib/owlCarousel/dist/assets/owl.theme.default.min.css'> -->
+
+
+    <!-- Firebase -->
+    <script src="https://www.gstatic.com/firebasejs/5.9.1/firebase-app.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include_once('./components/head-component.php') ?>
+<body>
+    <desktop-view>
+        <div class="home-container seletor">
+            <div class="col1-content">
+                <div class="foto-content"></div>
+            </div>
+            <div class="col2-content">
+                <h4>Olá, sou Fellipe Silvac ✌</h4>
+                <h6>Um designer que ama código e valorant.</h6>
+
+                <div class="home-descricao-content">
+                    <p>Product Design Intern na QConcursos.
+                    <br>Passado: Product Designer na DevMedia.</p>
+
+                    <p1>Sou um designer digital brasileiro. Eu amo design gráfico e sempre tente trazer referências para o digital experiência. Já trabalhei com Amazon, Havaianas, Petrobras, Vivo, FIEMG, Globo, Goldman Sachs, Santander, Cartão Elo, Algar Telecom, Flying Horse, Iveco, Usiminas, entre outras.</p1>
+                    
+                    <h4>2018-2020</h4>
+                </div>
+            </div>
+        </div>
+    </desktop-view>
+
+    <mobile-view>
+        <div class="home-container">
+            <h4>Olá, sou Fellipe Silvac ✌</h4>
+            <h6>Um designer que ama código e valorant.</h6>
+            <div class="foto-content"></div>
+            <div class="home-descricao-content">
+                <p>Product Design Intern na QConcursos.
+                <br>Passado: Product Designer na DevMedia.</p>
+
+                <p1>Sou um designer digital brasileiro. Eu amo design gráfico e sempre tente trazer referências para o digital experiência. Já trabalhei com Amazon, Havaianas, Petrobras, Vivo, FIEMG, Globo, Goldman Sachs, Santander, Cartão Elo, Algar Telecom, Flying Horse, Iveco, Usiminas, entre outras.</p1>
+                
+                <h4>2018-2020</h4>
+            </div>
+        </div>
+    </mobile-view>
+ 
+ 
+    <!-- Todos os Códigos JavaScript -->
+    <?php include_once('./components/script-component.php') ?>
+</body>
